2b384524b0cb22d78f4718957615849b58f1473e
Zack <zack@mateja.com>
2023-02-26 11:40:55 -0500

    First Solution Attempt

--- src/TwoSum/TwoSumMain.java
+++ src/TwoSum/TwoSumMain.java
@@ -1,4 +1,16 @@
 package TwoSum;
 
 public class TwoSumMain {
+    public int[] twoSum(int[] nums, int target) {
+        int[] sol = new int[2];
+        for (int i = 0; i < nums.length-1; i++){
+            for (int j = i+1; j < nums.length; j++){
+                if (nums[i] + nums[j] == target){
+                    sol = new int[]{i, j};
+                    return sol;
+                }
+            }
+        }
+        return sol;
+    }
 }
--- src/TwoSum/TwoSumSolution.java
+++ src/TwoSum/TwoSumSolution.java
@@ -1,4 +1,13 @@
 package TwoSum;
 
+import java.util.Arrays;
+
 public class TwoSumSolution {
+    public static void main(String[] args) {
+        TwoSumMain test = new TwoSumMain();
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        int[] solution = test.twoSum(nums,target);
+        System.out.println(Arrays.toString(solution));
+    }
 }
