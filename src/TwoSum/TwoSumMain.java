123cbddd9427532fec98de27f6ca8b2bef1304eb
Zack <zack@mateja.com>
2023-02-26 11:42:09 -0500

    Flipped Files

--- src/TwoSum/TwoSumMain.java
+++ src/TwoSum/TwoSumMain.java
@@ -1,16 +1,13 @@
 package TwoSum;
 
+import java.util.Arrays;
+
 public class TwoSumMain {
-    public int[] twoSum(int[] nums, int target) {
-        int[] sol = new int[2];
-        for (int i = 0; i < nums.length-1; i++){
-            for (int j = i+1; j < nums.length; j++){
-                if (nums[i] + nums[j] == target){
-                    sol = new int[]{i, j};
-                    return sol;
-                }
-            }
-        }
-        return sol;
+    public static void main(String[] args) {
+        TwoSumSolution test = new TwoSumSolution();
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        int[] solution = test.twoSum(nums,target);
+        System.out.println(Arrays.toString(solution));
     }
 }
--- src/TwoSum/TwoSumSolution.java
+++ src/TwoSum/TwoSumSolution.java
@@ -1,13 +1,16 @@
 package TwoSum;
 
-import java.util.Arrays;
-
 public class TwoSumSolution {
-    public static void main(String[] args) {
-        TwoSumMain test = new TwoSumMain();
-        int[] nums = {2,7,11,15};
-        int target = 9;
-        int[] solution = test.twoSum(nums,target);
-        System.out.println(Arrays.toString(solution));
+    public int[] twoSum(int[] nums, int target) {
+        int[] sol = new int[2];
+        for (int i = 0; i < nums.length-1; i++){
+            for (int j = i+1; j < nums.length; j++){
+                if (nums[i] + nums[j] == target){
+                    sol = new int[]{i, j};
+                    return sol;
+                }
+            }
+        }
+        return sol;
     }
 }
